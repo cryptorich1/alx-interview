509e9aaf86fb2d33a61004ed9f47a32521385d32
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-07-03 11:14:56 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,22 @@
+#!/usr/bin/python3
+
 def pascal_triangle(n):
     if n <= 0:
         return []
     
-    triangle = []
+    ptriangle = []
     
     for i in range(n):
-        row = [1]  # First element of each row is always 1
-        if triangle:  # If there are already rows in the triangle
-            prev_row = triangle[-1]  # Get the previous row
+        row = [1]
+        if ptriangle:
+            prev_row = ptriangle[-1]
             
             # Calculate the values for the current row
             for j in range(len(prev_row) - 1):
                 row.append(prev_row[j] + prev_row[j+1])
             
-            row.append(1)  # Last element of each row is always 1
+            row.append(1)
         
-        triangle.append(row)  # Add the current row to the triangle
+        ptriangle.append(row)
     
-    return triangle
+    return ptriangle
